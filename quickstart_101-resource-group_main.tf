353da95554a7eef97032d706b74758bfa59b2667
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-13 14:43:12 -0700

    Commit message

--- quickstart_101-resource-group_main.tf
+++ quickstart_101-resource-group_main.tf
@@ -1,20 +1,8 @@
-terraform {
-
-  required_version = ">=0.12"
-  
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
 resource "azurerm_resource_group" "rg" {
-  name = var.resource_group_name
   location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
